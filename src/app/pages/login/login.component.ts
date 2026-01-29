3915824ea95fb0121a3b6f45b1112b6432352282
esther <esther.magalhaes@estudante.ufla.br>
2026-01-29 14:17:54 -0300

    feat:

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,9 +1,8 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms'; // Importe o ReactiveFormsModule
 
 import { MatInputModule } from '@angular/material/input';
-import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
 
 @Component({
@@ -11,12 +10,30 @@ import { MatCardModule } from '@angular/material/card';
   standalone: true,
   imports: [
     CommonModule,
-    FormsModule,
+    ReactiveFormsModule, // Adicione aqui
     MatInputModule,
-    MatButtonModule,
     MatCardModule
   ],
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.scss']
 })
-export class LoginComponent {}
+export class LoginComponent {
+  loginForm: FormGroup; 
+
+  constructor(private fb: FormBuilder) {
+    this.loginForm = this.fb.group({
+      email: ['', [Validators.required, Validators.email]],
+      senha: ['', [Validators.required]]
+    });
+  }
+
+  login() {
+    if (this.loginForm.valid) {
+      console.log('Dados do login:', this.loginForm.value);
+    }
+  }
+
+  irParaHome() {
+    console.log('Navegando para home...');
+  }
+}
